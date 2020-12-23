47fd75ba273eed74a6c2a419bdd1b76733142130
SoaAlex <l.soaresalex@gmail.com>
2020-12-23 18:41:51 +0100

    redis fix password

--- src/redis-client.js
+++ src/redis-client.js
@@ -3,7 +3,7 @@ console.log("redis server should be at "+process.env.REDIS_URL)
 const client = redis.createClient({
     host: process.env.REDIS_URL,
     port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : null
+    password: process.env.REDIS_PASSWORD !== "undefined" ? process.env.REDIS_PASSWORD : null
 });
 
 module.exports =  client
