3f6b3edb7fe24bd0456d1f25d6c6966481941b9c
SoaAlex <l.soaresalex@gmail.com>
2020-12-23 14:56:16 +0100

    redis test

--- src/redis-client.js
+++ src/redis-client.js
@@ -1,5 +1,8 @@
 const redis = require('redis');
 console.log("redis server should be at "+process.env.REDIS_URL)
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient({
+    host: process.env.REDIS_URL,
+    port: process.env.REDIS_PORT
+});
 
 module.exports =  client
